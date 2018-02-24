a11d2b1f16195c0908c8e3f1a999401d44ddcc10
najeebael <najeebmms@hotmail.com>
2018-02-24 17:20:11 +0200

    Update media.py

--- media.py
+++ media.py
@@ -1,19 +1,17 @@
 import webbrowser
 
+
 class Movie():
-    #constructor for the movie class 
-    def __init__ (self, movie_title, movie_storyline, poster, movie_url):
-        #creating the instance variables of the class movie 
+    # constructor for the movie class
+    def __init__(self, movie_title, movie_storyline, poster, movie_url):
+        # creating the instance variables of the class movie
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster
         self.trailer_youtube_url = movie_url
 
+# instance method of the movie class, playing the trailer of your movie
 
-    #instance method of the movie class, playing the trailer of your movie
-    def show_trailer(self):
-        webbrowser.open(self.url)
-        
-        
-        
-    
+
+def show_trailer(self):
+    webbrowser.open(self.url)
